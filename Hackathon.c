43720cf0b6bb78c0f19d190b7a67dbb66ea37f8c
kingofgoodtimes365 <114881180+kingofgoodtimes365@users.noreply.github.com>
2022-10-03 04:00:28 +0800

    Add files via upload

---
+++ Hackathon.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+int main(void)
+{
+    int gascode;
+    scanf("%d\n", &gascode);
+    char windir;
+    scanf("%c", &windir);
+    if(windir=='w')
+    {
+    if(gascode==10891)
+    {
+              printf("Methyl isocyanate leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with alcohol resistant medium expansion foam which can be found in all emergency kits labelled AF.\n");
+              printf("Isolate spill to 100m\n");
+              printf("All people in Simei region are expected to head towards Pasir Ris away from the industry\n");
+              printf("Anyone exposed to the gas please avoid the use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact\n");
+    }
+    else if(gascode==10892)
+    {
+              printf("Styrene leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 50m.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("All people in Simei region are expected to head towards Pasir Ris away from the industry\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else if(gascode==10893)
+    {
+              printf("Acetylene leak detected");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 100m in all directions\n");
+              printf("All people in Simei region are expected to head towards Pasir Ris away from the industry atlease 500m from the industry.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else
+    {         printf("False Alarm");
+    }}
+    if(windir=='e')
+    {
+    if(gascode==10891)
+    {
+              printf("Methyl isocyanate leak detected");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with alcohol resistant medium expansion foam which can be found in all emergency kits labelled AF.\n");
+              printf("Isolate spill to 100m\n");
+              printf("All people in Simei region are expected to head towards Tuas Link away from the industry\n");
+              printf("Anyone exposed to the gas please avoid the use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact\n");
+    }
+    else if(gascode==10892)
+    {
+              printf("Styrene leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 50m.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("All people in Simei region are expected to head towards Tuas Link away from the industry\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else if(gascode==10893)
+    {
+              printf("Acetylene leak detected");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 100m in all directions\n");
+              printf("All people in Simei region are expected to head towards Tuas Link away from the industry atlease 500m from the industry.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else
+    {         printf("False Alarm");
+    }}
+    if(windir=='n')
+    {
+    if(gascode==10891)
+    {
+              printf("Methyl isocyanate leak detected");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with alcohol resistant medium expansion foam which can be found in all emergency kits labelled AF.\n");
+              printf("Isolate spill to 100m\n");
+              printf("All people in Simei region are expected to head towards Tiong Bahru away from the industry\n");
+              printf("Anyone exposed to the gas please avoid the use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact\n");
+    }
+    else if(gascode==10892)
+    {
+              printf("Styrene leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 50m.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("All people in Simei region are expected to head towards Tiong Bahru away from the industry\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else if(gascode==10893)
+    {
+              printf("Acetylene leak detected");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 100m in all directions\n");
+              printf("All people in Simei region are expected to head towards Tiong Bahru away from the industry atlease 500m from the industry.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else
+    {         printf("False Alarm\n");
+    }}
+        if(windir=='s')
+    {
+    if(gascode==10891)
+    {
+              printf("Methyl isocyanate leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with alcohol resistant medium expansion foam which can be found in all emergency kits labelled AF.\n");
+              printf("Isolate spill to 100m\n");
+              printf("All people in Simei region are expected to head towards Sembawang away from the industry\n");
+              printf("Anyone exposed to the gas please avoid the use of water for any burning sensations and do not turn on ignition sources");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact\n");
+    }
+    else if(gascode==10892)
+    {
+              printf("Styrene leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 50m.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("All people in Simei region are expected to head towards Sembawang away from the industry\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else if(gascode==10893)
+    {
+              printf("Acetylene leak detected\n");
+              printf("Gas Highly Flammable. Turn off all ignition sources immediately\n");
+              printf("All personnel on site are requested to wear self-contained breathing apparatus before trying to stop the leak\n");
+              printf("All personnel, any fire must be put off with regular foam which can be found in all emergency kits labelleled F.\n");
+              printf("Isolate spill to 100m in all directions\n");
+              printf("All people in Simei region are expected to head towards Sembawang away from the industry atlease 500m from the industry.Consider initial downwind evacuation for at least 800 meters\n");
+              printf("Anyone exposed to the gas please use of water for any burning sensations and do not turn on ignition sources\n");
+              printf("All people affected must be administered oxygen externally.Please avoid physical contact.Move victim to fresh air if it can be done safely.\n");
+    }
+    else
+    {         printf("False Alarm\n");
+    }}
+    return 0;
+    }
+
+
